d3e3e464b597e18490884e011834e1641003aac5
Kranthikumar96 <bandarukranthikumar96@gmail.com>
2025-10-28 06:18:01 -0700

    terraform aws security group

--- locals.tf
+++ locals.tf
@@ -1,9 +1,7 @@
 locals {
   common_tags = {
-    project = var.project
+    Project = var.project
     Environment= var.environment
-    terraform = "true"
+    Terraform = "true"
   }
-
-  az_names = slice(data.aws_availability_zones.available.names, 0, 2)
 }
